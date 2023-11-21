c210736f897e38fd5df42c6a5c661486c3ce01fb
Eder Sena <eder.sena@live.com>
2023-11-21 10:43:54 -0300

    Refactor Profile.vue component

---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="image-gallery-container">
+    <img v-for="post in posts" :src="post.image" :key="post.id" alt="" />
+  </div>
+</template>
+
+<script setup>
+const { posts } = defineProps(["posts"]);
+</script>
+
+<style scoped>
+.image-gallery-container {
+  display: flex;
+  justify-content: left;
+  flex-wrap: wrap;
+}
+
+img {
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+  margin: 5px;
+}
+</style>
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,7 +1,29 @@
 <template>
   <Container>
     <div class="profile-container">
-      <UserBar username="Ederusena" :userInfo="{posts: 4, followers: 100, following: 344}"/>
+      <UserBar
+        username="Ederusena"
+        :userInfo="{ posts: 4, followers: 100, following: 344 }"
+      />
+      <ImageGallery
+        :posts="[
+          {
+            id: 1,
+            image:
+              'https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1600',
+          },
+          {
+            id: 2,
+            image:
+              'https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=1600',
+          },
+          {
+            id: 3,
+            image:
+              'https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&w=1600',
+          },
+        ]"
+      />
     </div>
   </Container>
 </template>
@@ -9,9 +31,9 @@
 <script setup>
 import { useRoute } from "vue-router";
 import Container from "./Container.vue";
+import ImageGallery from "./ImageGallery.vue";
 import UserBar from "./UserBar.vue";
 const route = useRoute();
-
 </script>
 
 <style scoped>
